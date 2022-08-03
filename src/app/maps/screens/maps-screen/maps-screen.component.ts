3ac4ecc6304982a2ee305a77e390b68182dd2703
JoBelmar <jose@iot4life>
2022-08-02 22:25:48 -0400

    Primer Commit

---
+++ src/app/maps/screens/maps-screen/maps-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maps-screen',
+  templateUrl: './maps-screen.component.html',
+  styleUrls: ['./maps-screen.component.css']
+})
+export class MapsScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
